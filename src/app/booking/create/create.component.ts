65e35400c92a94b079c54690276f450bfcd52327
BrandonVanek <bvanek@skillstorm.com>
2022-08-17 11:16:14 -0400

    bug fixing

--- src/app/booking/create/create.component.ts
+++ src/app/booking/create/create.component.ts
@@ -4,6 +4,8 @@ import { Router } from '@angular/router';
 import { FormGroup, FormControl, Validators} from '@angular/forms';
 import { FlightService } from 'src/app/flight/flight.service';
 import { PassengerService } from 'src/app/passenger/passenger.service';
+import { Flight } from "../../flight/flight";
+import { Passenger } from 'src/app/passenger/passenger';
      
 @Component({
   selector: 'app-create',
@@ -13,13 +15,18 @@ import { PassengerService } from 'src/app/passenger/passenger.service';
 export class CreateComponent implements OnInit {
     
   form!: FormGroup;
+  max!: boolean;
+  passengersAndFlights: boolean;
 
   constructor(
     public bookingService: BookingService,
     public flightService: FlightService,
     public passengerService: PassengerService,
     private router: Router
-  ) { } 
+  ) { 
+    this.max = false;
+    this.passengersAndFlights = false;
+  } 
     
   ngOnInit(): void {
     this.form = new FormGroup({
@@ -31,19 +38,55 @@ export class CreateComponent implements OnInit {
     });
   }
 
+  ngOnChanges(): void {
+    this.max = false; 
+  }
+
   get f() { return this.form.controls; }
 
 
   submit(){
-    const genRanHex = (size: number) => [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join('');
-    this.form.value["confirmationNumber"] = genRanHex(16);
-    this.form.value["flight"] = this.flightService.getFlight(this.form.value["flightId"]);
-    this.form.value["passenger"] = this.passengerService.getPassenger(this.form.value["passengerId"]);
-    console.log(this.form.value);
-    console.log(this.form.valid);
-    this.bookingService.createBooking(this.form.value).subscribe(() => {
-      console.log("Booking created successfully!");
-      this.router.navigateByUrl('booking/index');
+    this.flightService.getFlights().subscribe((res: Flight[]) => {
+      // console.log(res.length)
+      if (res.length > 0) {
+        this.passengerService.getPassengers().subscribe((res: Passenger[]) => {
+          // console.log(res.length)
+          if (res.length > 0) {
+            this.passengersAndFlights = true;
+            const genRanHex = (size: number) => [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join('');
+            this.form.value["confirmationNumber"] = genRanHex(16);
+            this.flightService.getFlight(this.form.value["flightId"])
+              .subscribe((response: Flight) => {
+                if (response.bookings.length >= response.maxCapacity) {
+                  this.max = true;
+                  this.form.controls['flight'].markAsTouched();
+                  // console.log(this.max);
+                  alert("This flight has reached its maximum capacity, please choose a different flight!");
+                  return;
+                }
+                else {
+                  // console.log(this.max);
+                  this.form.value["flight"] = this.flightService.getFlight(this.form.value["flightId"]);
+                  this.form.value["passenger"] = this.passengerService.getPassenger(this.form.value["passengerId"]);
+                  // console.log(this.form.value);
+                  // console.log(this.form.valid);
+                  this.bookingService.createBooking(this.form.value).subscribe(() => {
+                    console.log("Booking created successfully!");
+                    this.router.navigateByUrl('booking/index');
+                  }, error => alert("Passenger NOT FOUND choose an existing PassengerId\n" + error.message));
+                  this.max = false;
+                  return;
+                }
+              }, error => alert("Flight NOT FOUND choose an existing FlightId\n" + error.message));
+          }
+          else {
+            alert("There must be at least one passenger and one flight in the database to make a booking!");
+          }
+        });
+      }          
+      else {
+        alert("There must be at least one passenger and one flight in the database to make a booking!");
+      }
     });
   }
   
